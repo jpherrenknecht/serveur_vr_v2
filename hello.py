9ad2fc54102585eec1dc236261df3a3c239f42ac
jpherrenknecht <jpherrenknecht@gmail.com>
2020-09-11 10:15:48 +0200

    Mise en place

---
+++ hello.py
@@ -0,0 +1,12 @@
+import numpy as np
+import os
+import time
+import math
+import numpy as np
+from urllib.request import urlretrieve
+import xarray as xr
+import h5py
+from datetime import datetime
+from scipy.interpolate import RegularGridInterpolator
+from pathlib import Path
+print("hello")
